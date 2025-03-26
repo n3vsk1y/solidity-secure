13898082c7ade462568414b6fe2af33695f669a6
n3vsk1y <alexafanasenko900@gmail.com>
2025-03-27 00:37:27 +0300

    transfer contract file: front -> back

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+import os
 
 app = FastAPI()
 
-@app.get('/')
-async def hello():
-    return {"App": "Started"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.post("/api/upload")
+async def upload_contract(file: UploadFile = File(...)):
+    if not file.filename.endswith(".sol"):
+        return {"error": "Только файлы .sol разрешены"}
+    
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    return {"message": "Файл загружен", "filename": file.filename}
--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
-    withCredentials: true,
+    baseURL: 'http://127.0.0.1:8000/api'
 });
 
 export async function sendContractFile(file) {
